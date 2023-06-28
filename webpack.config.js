a42bb09545966a260c5189e05f747125bb9af706
Leander Studer <leander.studer@gmail.com>
2023-06-28 17:41:28 -0500

    add parallel watcher
    implement parallel watcher for webpack and sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     devtool: "inline-source-map",
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist/js"),
+        clean: true,
     },
 };
